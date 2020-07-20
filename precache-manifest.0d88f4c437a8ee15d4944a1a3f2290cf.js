71b461c7c1a8867dcf8de2046c16c5a8b2a5d133
Helen Sabrina Braz <helsabrinacarvalho@hotmail.com>
2020-07-19 23:46:19 -0300

    Updates

---
+++ precache-manifest.0d88f4c437a8ee15d4944a1a3f2290cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f4417f4cb716915b3384feb2d449ea",
+    "url": "/ChallengeReactJS/index.html"
+  },
+  {
+    "revision": "45e041e308d4a2311196",
+    "url": "/ChallengeReactJS/static/js/2.b34a29d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ChallengeReactJS/static/js/2.b34a29d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e574c3cc340573afa8",
+    "url": "/ChallengeReactJS/static/js/main.10b66c77.chunk.js"
+  },
+  {
+    "revision": "83d3f57184cbe0b07fbd",
+    "url": "/ChallengeReactJS/static/js/runtime-main.0082c3f9.js"
+  },
+  {
+    "revision": "407ed75dd485ba5ed45c0d9c818f1657",
+    "url": "/ChallengeReactJS/static/media/hot.407ed75d.svg"
+  },
+  {
+    "revision": "764ceaa5ac821368d9c34f3db32342a3",
+    "url": "/ChallengeReactJS/static/media/logo.764ceaa5.svg"
+  },
+  {
+    "revision": "262be12c458a577d3c48901d52ca4b8a",
+    "url": "/ChallengeReactJS/static/media/news.262be12c.svg"
+  },
+  {
+    "revision": "d0096c51bd22b481109f1463bc1e46c9",
+    "url": "/ChallengeReactJS/static/media/rising.d0096c51.svg"
+  }
+]);
